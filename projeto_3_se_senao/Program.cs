11f0722eccaade55648f6960f22f5b28f5ee079a
luizao1ds <luizgomesdossantos.ds@gmail.com>
2026-06-28 22:17:55 -0300

    Atualização 2.0.0
    Tudo pronto e configurado

---
+++ projeto_3_se_senao/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.Write("Qual é sua a nota?: ");
+double nota= double.Parse(Console.ReadLine());
+
+if (nota >= 7)
+{
+    Console.WriteLine("Aprovado!");
+}
+else if (nota >= 5)
+{
+    Console.WriteLine("Em recuperação.");
+}
+else
+{
+    Console.WriteLine("Reprovado.");
+}
