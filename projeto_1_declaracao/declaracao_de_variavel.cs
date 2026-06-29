63db076b9fd263e4c09c4a9bc4c270582539ed75
luizao1ds <luizgomesdossantos.ds@gmail.com>
2026-06-28 21:13:23 -0300

    Atualização 1.0.2
    entendendo dotnet

---
+++ projeto_1_declaracao/declaracao_de_variavel.cs
@@ -0,0 +1,17 @@
+﻿string mensagem = "Hello, Word!"; //string = Textos ou sequências de caracteres, sempre entre aspas duplas
+int n1 = 100; //int = Números inteiros
+double n2 = 3.14159265358979323846264338327950288419716939937510; //double / float = Números com casas decimais
+char l = 'l'; //char = Um único caractere, entre aspas simples
+bool v = true; //bool = Valores verdadeiro ou falso (ex: true, false)
+
+Console.WriteLine(mensagem);
+ Thread.Sleep(1000); // Faz demorar 2000 milissegundos (2s) para a proxima ação acontecer
+Console.WriteLine(n1);
+ Thread.Sleep(1000); 
+Console.WriteLine(n2);
+ Thread.Sleep(1000); 
+Console.WriteLine(l);
+ Thread.Sleep(1000); 
+Console.WriteLine(v);
+ Thread.Sleep(1000); 
+Console.Clear() ; // limpa o terminal após as ações serem concluidas
---
+++ projeto_2_se_senao_scanf/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Qual é o seu nome?: ");
+string nome = Console.ReadLine();
+
+
+Console.Write("Qual é sua a idade?: ");
+double idade = double.Parse(Console.ReadLine());
+
+Console.Write("Olá jovem " + nome +" de " + idade + " de idade\n\n");
+
+Console.Write("digite v para verdadeiro e f para falso\n");
+
+Console.Write("Você ama o programador incrivel que fez esse código?: ");
+bool c;
+
+string resposta = Console.ReadLine();
+
+c = resposta == "v";
+
+if (c)
+{
+Console.Write("Muito obrigado!");
+}
+else
+{
+Console.Write("Estou triste");
+}
