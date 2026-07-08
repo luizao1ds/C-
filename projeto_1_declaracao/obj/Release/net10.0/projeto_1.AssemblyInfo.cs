6dbbab85e32795377327d068828ad230425b876c
luizao1ds <luizgomesdossantos.ds@gmail.com>
2026-07-08 16:03:28 -0300

    atualização 2.0.0
    Mudando para o VS

--- projeto_1_declaracao/obj/Release/net10.0/projeto_1.AssemblyInfo.cs
+++ projeto_1_declaracao/obj/Release/net10.0/projeto_1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto_1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f0722eccaade55648f6960f22f5b28f5ee079a")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto_1")]
---
+++ projeto_4_lacos_de_repeticao/for.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace projeto_4_lacos_de_repeticao
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Digite o comando desejado: for, while, do while ou Calculadora.");
+          string comando = Console.ReadLine();
+
+            if (comando == "for")
+            {
+                PrimeiroFor();
+
+                  Thread.Sleep(5000);
+
+            Console.Clear();
+            }
+            else if (comando == "while")
+            {
+                PrimeiroWhile();
+                  Thread.Sleep(5000);
+
+            Console.Clear();
+            }
+            else if (comando == "do while")
+            {
+                PrimeiroDoWhile();
+                Thread.Sleep(5000);
+                Console.Clear();
+            }
+            else if (comando == "Calculadora")
+            {
+                calculadora();
+            }     
+            else
+            {
+                Console.WriteLine("Comando inválido");
+                  Thread.Sleep(5000);
+
+            Console.Clear();
+            }
+             
+        }
+
+        static void PrimeiroFor()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Número: {i}");
+            }
+        }
+
+        static void PrimeiroWhile()
+        {
+            int j = 1;
+
+            while (j < 5)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+
+        static void PrimeiroDoWhile()
+        {
+            int j = 1;
+
+            do
+            {
+                Console.WriteLine("oi");
+                j++;
+            }
+            while (j < 5);
+        }
+
+         static void calculadora()
+         {
+               Console.WriteLine("Digite o primeiro número: ");
+               int num1 = Convert.ToInt32(Console.ReadLine());
+   
+               Console.WriteLine("Digite o segundo número: ");
+               int num2 = Convert.ToInt32(Console.ReadLine());
+   
+               Console.WriteLine("Escolha a operação desejada: +, -, *, /, %");
+               string operacao = Console.ReadLine();
+   
+               switch (operacao)
+               {
+                  case "+":
+                     Console.WriteLine($"Resultado: {num1 + num2}");
+                     break;
+                  case "-":
+                     Console.WriteLine($"Resultado: {num1 - num2}");
+                     break;
+                  case "*":
+                     Console.WriteLine($"Resultado: {num1 * num2}");
+                     break;
+                  case "/":
+                     if (num2 != 0)
+                     {
+                           Console.WriteLine($"Resultado: {num1 / num2}");
+                     }
+                     else
+                     {
+                           Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                     }
+                     break;
+                  case "%":
+                     if (num2 != 0)
+                     {
+                           Console.WriteLine($"Resultado: {num1 % num2}");
+                     }
+                     else
+                     {
+                           Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                     }
+                      break;
+                  default:
+                     Console.WriteLine("Operação inválida.");
+                     break;
+               }
+    }
+  }
+
+
+}
+
